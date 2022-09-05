88e18912db254d618a93b2867369da8e9f4c24de
Jelena Djurdjevic <+>
2022-09-05 10:00:49 -0600

    commit tree f1c832ddf554054bd0a7edcbe15f7817c1b43fcd

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(){
+    const newCat = [...cats, 'Broom'];
+    return newCat;
+}
+function prependCat(){
+    const newCat = ['Arnold',...cats];
+    return newCat;
+}
+function removeLastCat(){
+    const newCat = cats.slice(0,-1);
+    return newCat;
+}
+function removeFirstCat(){
+    const newCat = cats.slice(1);
+    return newCat;
+}
